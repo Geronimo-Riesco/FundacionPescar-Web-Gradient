de42fe14481634af15b3fdce146b28d48c497e1f
Gerónimo Ezequiel Riesco <geroriesco2010@hotmail.com>
2022-08-06 02:25:03 -0300

    JavaScript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.home');
-  ScrollReveal().reveal('#intro', { delay: 500 });
-  ScrollReveal().reveal('.features-card', { delay: 500 });
-  ScrollReveal().reveal('.contact-details', { delay: 500 });
-  ScrollReveal().reveal('#social', { delay: 500 });
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal(".home");
+ScrollReveal().reveal("#intro", { delay: 400 });
+ScrollReveal().reveal(".features-card", { delay: 400 });
+ScrollReveal().reveal(".contact-details", { delay: 400 });
